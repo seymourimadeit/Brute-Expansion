8e0f81ae2bb571a4b15629fe5960535f6444830a
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-09-26 21:38:09 -0700

    Initial commit

---
+++ src/main/java/tallestegg/bruteexpansion/BruteExpansion.java
@@ -0,0 +1,36 @@
+package tallestegg.bruteexpansion;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BruteExpansion.MODID)
+public class BruteExpansion {
+    
+    public static final String MODID = "bruteexpansion";
+
+    public BruteExpansion() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+    }
+}
